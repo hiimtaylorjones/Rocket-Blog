bad3871f4fe09585b475030156f97cc636587172
hiimtaylorjones <taylorjonessoftware@gmail.com>
2019-06-03 21:00:50 -0400

    evening up serde imports

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 extern crate rust_blog;
 extern crate diesel;
 #[macro_use] extern crate rocket;
-#[macro_use] extern crate serde_derive;
+#[macro_use] extern crate serde;
 extern crate rocket_contrib;
 
 
